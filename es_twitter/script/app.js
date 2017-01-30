dd1b7d34a15dd8c9b23edd3e826a4cf07659b3ba
ColmeSH <mauriziocontatto@gmail.com>
2017-01-30 20:13:52 +0100

    tweeter ok

--- es_twitter/script/app.js
+++ es_twitter/script/app.js
@@ -1,27 +1,47 @@
 $(document).ready(function(){
-    
-    
-    //ad ogni tasto che premo della testarea
-    i = 140;
-    $("textarea").keypress(function(){
-        //dig += 0;
-        dig = $("p").text(i -= 1);
-        
-        
-        
-       
-       if(dig >= 140){
-       
-		    	$("a").click(function(){
-					
-					$("textarea").text();//valore textarea       
-		    		//console.log(txt);
-		    		/*$("ul").append(txt);
-		    		txt = "";*/
-				});
-		};
-	});  
-});
-    
-    
 
+
+
+    var i = 140;
+    //for each keypress decrement the counter
+    $("textarea").keypress(function(){
+
+      var tot = $("p").text(i -= 1);
+      //console.log();
+      if(i < 0){
+        $(".counter").css({
+          "color":"red",
+        });
+        //remove the button
+        $("a").css({
+          "background-color":"gray"
+        });
+        $("a").off();
+      }
+    });
+
+
+
+    $(".btn").click(function(btn){
+      //console.log(i);// button ok
+      var txt = $("textarea").val();
+      //console.log(txt);//ho il valore
+        $("ul").append("<li><img src='account.png' width='30px'>  " + txt + "</li><br />");
+        //position
+          $(".posts").css({
+            "width":"400px",
+            "position":"relative",
+            "left":"350px",
+            "top":"50px",
+            "opacity":"0.4",
+
+
+          });
+        $("textarea").val("");
+        i = 140;//return val init
+
+
+
+    });
+
+});
